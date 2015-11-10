7eaaa8c912a8520cb09f6386a0f96df48085525d
Vincent Trumpff <vincent@stoic.com>
2015-11-10 21:52:55 +0100

    button, state binary, state 0-100

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  UiButton      : require('./lib/ui-button'),
+  UiStateBinary : require('./lib/ui-state-binary'),
+  UiState0_100  : require('./lib/ui-state-0-100')
+};
