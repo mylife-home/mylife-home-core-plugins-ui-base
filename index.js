15ff2068b88dac4b816d316744d3a92f2eab7728
Vincent Trumpff <vincent@stoic.com>
2015-12-23 15:19:26 +0100

    bug fix

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 module.exports = {
   UiButton       : require('./lib/ui-button'),
   UiStateBinary  : require('./lib/ui-state-binary'),
-  UiStatePercent : require('./lib/ui-state-precent')
+  UiStatePercent : require('./lib/ui-state-percent')
 };
