349c3fe0c72ab738fba636047da1002937fe3a6e
Vincent Trumpff <vincent@stoic.com>
2016-11-05 15:19:30 +0100

    add ui-state-temperature

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-  UiButton       : require('./lib/ui-button'),
-  UiStateBinary  : require('./lib/ui-state-binary'),
-  UiStatePercent : require('./lib/ui-state-percent')
+  UiButton           : require('./lib/ui-button'),
+  UiStateBinary      : require('./lib/ui-state-binary'),
+  UiStatePercent     : require('./lib/ui-state-percent'),
+  UiStateTemperature : require('./lib/ui-state-temperature')
 };
